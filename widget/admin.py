69017f7d126126e7577149d6267c170a277e4873
Denzi666 <klychkov_s.66@mail.ru>
2026-04-02 15:53:31 +0400

    Register Company in admin

--- widget/admin.py
+++ widget/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Callback
+from .models import Callback, Company
 
 # Register your models here.
 admin.site.register(Callback)
+admin.site.register(Company)
