48d9ad982811278b67a823912cd2468e73578696
Denzi666 <klychkov_s.66@mail.ru>
2026-04-02 15:52:03 +0400

    Add Company model and relationship

--- widget/models.py
+++ widget/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название компании")
+    api_key = models.CharField(max_length=100, unique=True, verbose_name="Уникальный API ключ")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата регистрации")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Компания"
+        verbose_name_plural = "Компании"
+
 class Callback(models.Model):
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+        related_name='callbacks',
+        verbose_name='Компания',
+        null=True,
+        blank=True
+    )
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__ (self):
         return f"Заявка от {self.name}"
+    
