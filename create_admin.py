0302856effa9a8c79e277ce3dacbdf985d420964
Denzi666 <klychkov_s.66@mail.ru>
2026-04-02 15:02:55 +0400

    Added admin data

---
+++ create_admin.py
@@ -0,0 +1,18 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings') # Проверь, что твой проект называется mysite
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = 'admin'
+email = 'klychkov_s.66@mail.ru'  # <-- Впиши сюда свою почту
+password = '25092003Ss@'          # <-- Впиши сюда свой пароль
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Суперпользователь успешно создан!")
+else:
+    print("Суперпользователь уже существует.")
